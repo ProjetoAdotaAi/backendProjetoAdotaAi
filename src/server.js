5e3c3d2a51ac542a82a6bd408db18d2a56a72965
Kainak <natanfkainak@gmail.com>
2025-06-02 19:31:09 -0300

    ajuste swagger -> inserido no gitignore

--- src/server.js
+++ src/server.js
@@ -11,9 +11,13 @@ const error = (err) => {
 
 const listening = () => {
   console.log(`Server running on port ${process.env.PORT}`);
+  // console.log(`Server running on http://192.168.100.140:${process.env.PORT || 4040}`);
 };
 
 const server = http.createServer(app);
 server.listen(process.env.PORT || 4040);
+// server.listen(process.env.PORT || 4040, '0.0.0.0', () => {
+//   console.log(`Server is listening on http://192.168.100.140:${process.env.PORT || 4040}`);
+// });
 server.on("error", error);
 server.on("listening", listening);
